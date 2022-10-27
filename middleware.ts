3ac6c2cd066ea35c7e8a050970f15bca438459f9
Anton Palermo <ANPBusinessmail@gmail.com>
2022-10-27 19:54:02 +0800

    fix: middlware error

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
 export { default } from 'next-auth/middleware'
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|static|favicon.ico).*)'
+  ]
+}
